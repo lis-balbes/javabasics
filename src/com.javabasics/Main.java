732581db308fbde3745ecb481cf4756d7ee5b50a
NK <anastasiia.kapanina@gmail.com>
2017-12-01 20:08:16 +0300

    Looks like gitignore fail

--- src/com.javabasics/Main.java
+++ src/com.javabasics/Main.java
@@ -1,8 +1,20 @@
 package com.javabasics;
 
+import com.javabasics.Factorial;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+
+        //int value = Integer.parseInt(args[0]);
+
+        int[] a1 = new int[]{1, 2, 3, 5};
+        int[] a2 = new int[]{1, 4};
+        int[] result = MergeArraysLinear.mergeArrays(a1, a2);
+
+        System.out.println(Arrays.toString(result));
+
     }
 }
